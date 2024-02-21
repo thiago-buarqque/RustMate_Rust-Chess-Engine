a2ca14ad7ba1f90019c7cb6feee78210e930af0b
thiago-buarqque <thiago.buarqque@gmail.com>
2024-02-21 00:42:55 -0300

    #6 Adding move highlight

--- web-application/src/Board/Board.tsx
+++ web-application/src/Board/Board.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import BoardPiece, { PIECE_ICONS } from "./BoardPiece";
+import { PIECE_ICONS } from "./BoardPiece";
 import { AIResponse, TBoard, TMove, TPiece, TPieceColor, TPieceType } from "./types";
 
 //@ts-ignore
@@ -11,337 +11,293 @@ import moveAudio from "../assets/sound/move-self.mp3";
 import http from "../http-common";
 
 import "./board.scss";
-import { EMPTY_FEN, INITIAL_FEN } from "./constants";
+import { EMPTY_FEN, EMPTY_PIECE, INITIAL_FEN, LINES } from "./constants";
 import Logs, { getBoardEvaluationMessage } from "./Logs";
-
-const LINES = [0, 1, 2, 3, 4, 5, 6, 7];
-const COLUMNS: { [key: number]: string } = {
-  0: "A",
-  1: "B",
-  2: "C",
-  3: "D",
-  4: "E",
-  5: "F",
-  6: "G",
-  7: "H",
-};
-
-const EMPTY_PIECE: TPiece = {
-  moves: [],
-  position: -1,
-  value: 0,
-  fen: null,
-  white: false,
-};
+import Cell from "./Cell";
 
 const get_empty_piece = (position: number) => {
-  const piece: TPiece = JSON.parse(JSON.stringify(EMPTY_PIECE));
-  piece.position = position;
+	const piece: TPiece = JSON.parse(JSON.stringify(EMPTY_PIECE));
+	piece.position = position;
 
-  return piece;
+	return piece;
 };
 
 const playMoveAudio = (capture: boolean) => {
-  let audio;
+	let audio;
 
-  if (capture) {
-    audio = new Audio(captureAudio);
-  } else {
-    audio = new Audio(moveAudio);
-  }
-  audio.play();
+	if (capture) {
+		audio = new Audio(captureAudio);
+	} else {
+		audio = new Audio(moveAudio);
+	}
+	audio.play();
 };
 
 const getPieceMove = (availableMoves: TMove[], position: number) => {
-  return availableMoves.find((move) => move.toPosition === position);
+	return availableMoves.find((move) => move.toPosition === position);
 };
 
 const Board = () => {
-  const [selectedPiece, setSelectedPiece] = useState<TPiece | null>(null);
-  const [isWaitingForAI, setIsWaitingForAI] = useState(false);
-  const [lastAIResponse, setLastAIResponse] = useState<AIResponse>();
-  const [board, setBoard] = useState<TBoard>({
-    blackCaptures: [],
-    blackKingInCheck: false,
-    boardEvaluation: 0,
-    boardFen: INITIAL_FEN,
-    whiteCaptures: [],
-    pieces: [],
-    whiteKingInCheck: false,
-    whiteMove: true,
-    winner: "-",
-    zobrit: 0
-  });
+	const [selectedPiece, setSelectedPiece] = useState<TPiece | null>(null);
+	const [lastMove, setLastMove] = useState<TMove | null>(null);
+	const [isWaitingForAI, setIsWaitingForAI] = useState(false);
+	const [lastAIResponse, setLastAIResponse] = useState<AIResponse>();
+	const [board, setBoard] = useState<TBoard>({
+		blackCaptures: [],
+		blackKingInCheck: false,
+		boardEvaluation: 0,
+		boardFen: INITIAL_FEN,
+		whiteCaptures: [],
+		pieces: [],
+		whiteKingInCheck: false,
+		whiteMove: true,
+		winner: "-",
+		zobrit: 0,
+	});
 
-  const onPieceSelect = (piece: TPiece) => {
-    if(isWaitingForAI) {
-      return;
-    }
+	const onPieceSelect = (piece: TPiece) => {
+		if (isWaitingForAI) {
+			return;
+		}
 
-    if (board.whiteMove !== piece.white) {
-      // Play invalid move sound
-      return;
-    }
-    if (selectedPiece === piece) {
-      setSelectedPiece(null);
-    } else {
-      if (selectedPiece) {
-        togglePieceAvailableMoves(selectedPiece);
-      }
+		if (board.whiteMove !== piece.white) {
+			// Play invalid move sound
+			return;
+		}
+		if (selectedPiece === piece) {
+			setSelectedPiece(null);
+		} else {
+			if (selectedPiece) {
+				togglePieceAvailableMoves(selectedPiece);
+			}
 
-      setSelectedPiece(piece);
-    }
-    togglePieceAvailableMoves(piece);
-  };
+			setSelectedPiece(piece);
+		}
+		togglePieceAvailableMoves(piece);
+	};
 
-  const togglePieceAvailableMoves = (piece: TPiece) => {
-    if(isWaitingForAI) {
-      return;
-    }
+	const togglePieceAvailableMoves = (piece: TPiece) => {
+		if (isWaitingForAI) {
+			return;
+		}
 
-    piece.moves.forEach((move) => {
-      const className =
-        board.pieces[move.toPosition].fen !== EMPTY_FEN ? "capture-receptor" : "empty-receptor";
+		piece.moves.forEach((move) => {
+			const className = board.pieces[move.toPosition].fen !== EMPTY_FEN ? "capture-receptor" : "empty-receptor";
 
-      const cell = document.querySelector(`.cell[data-pos='${move.toPosition}']`) as HTMLDivElement;
+			const cell = document.querySelector(`.cell[data-pos='${move.toPosition}']`) as HTMLDivElement;
 
-      // cell.onclick = () => onCellClick(cell, move.row, move.column);
-      cell.classList.toggle(className);
+			// cell.onclick = () => onCellClick(cell, move.row, move.column);
+			cell.classList.toggle(className);
 
-      const cellPiece = document.querySelector(
-        `.cell[data-pos='${move.toPosition}'] button.piece-button`
-      ) as HTMLDivElement;
+			const cellPiece = document.querySelector(
+				`.cell[data-pos='${move.toPosition}'] button.piece-button`
+			) as HTMLDivElement;
 
-      cellPiece?.classList.toggle("disabled");
-    });
-  };
+			cellPiece?.classList.toggle("disabled");
+		});
+	};
 
-  const onMovePiece = (cell: HTMLDivElement, cellPosition: number) => {
-    if(isWaitingForAI) {
-      return;
-    }
+	const onMovePiece = (cell: HTMLDivElement, cellPosition: number) => {
+		if (isWaitingForAI) {
+			return;
+		}
 
-    if (selectedPiece) {
-      const { position, moves } = selectedPiece;
+		if (selectedPiece) {
+			const { position, moves } = selectedPiece;
 
-      let pieceMove = getPieceMove(moves, cellPosition);
+			let pieceMove = getPieceMove(moves, cellPosition);
 
-      if (pieceMove === undefined) {
-        return;
-      }
+			if (pieceMove === undefined) {
+				return;
+			}
 
-      // TODO: Add the option to choose the promotion
-      if (pieceMove.promotion) {
-        pieceMove.promotionType = TPieceColor.White | TPieceType.Queen
-      }
+			// TODO: Add the option to choose the promotion
+			if (pieceMove.promotion) {
+				pieceMove.promotionType = TPieceColor.White | TPieceType.Queen;
+			}
 
-      const copy_board: TBoard = JSON.parse(JSON.stringify(board));
+			const copy_board: TBoard = JSON.parse(JSON.stringify(board));
 
-      copy_board.pieces[position] = get_empty_piece(position);
+			copy_board.pieces[position] = get_empty_piece(position);
 
-      selectedPiece.position = cellPosition;
+			selectedPiece.position = cellPosition;
 
-      copy_board.pieces[cellPosition] = selectedPiece;
+			copy_board.pieces[cellPosition] = selectedPiece;
 
-      setSelectedPiece(null);
-      setBoard(copy_board);
+			setSelectedPiece(null);
+			setBoard(copy_board);
 
-      const cellPiece = document.querySelector(
-        `.cell[data-pos='${position}'] button.piece-button.disabled`
-      ) as HTMLDivElement;
+			const cellPiece = document.querySelector(
+				`.cell[data-pos='${position}'] button.piece-button.disabled`
+			) as HTMLDivElement;
 
-      cellPiece?.classList.remove("disabled");
+			cellPiece?.classList.remove("disabled");
 
-      // document.querySelectorAll('.cell.to-position').forEach(el => el.classList.remove('to-position'));
+			playMoveAudio(pieceMove.capture);
 
-      // cell?.classList.add("to-position");
+			togglePieceAvailableMoves(selectedPiece);
 
-      playMoveAudio(pieceMove.capture);
+			movePiece(pieceMove);
+			setLastMove(pieceMove);
+		}
+	};
 
-      togglePieceAvailableMoves(selectedPiece);
-      
-      movePiece(pieceMove);
-    }
-  };
+	const fetchBoard = async () => {
+		return http
+			.get<TBoard>("/board")
+			.then((response) => response.data)
+			.then((data) => {
+				setBoard(data);
+			});
+	};
 
-  const fetchBoard = async () => {
-    return http
-      .get<TBoard>("/board")
-      .then((response) => response.data)
-      .then((data) => {        
-        setBoard(data);
-      });
-  }
+	const getAiMove = () => {
+		setIsWaitingForAI(true);
 
-  const getAiMove = () => {
-    setIsWaitingForAI(true);
-    
-    http
-      .post<AIResponse>("/ai/move")
-      .then((response) => response.data)
-      .then((aiResponse) => {
-        fetchBoard().then(() => {
-          playMoveAudio(aiResponse.aiMove.capture);
+		http.post<AIResponse>("/ai/move")
+			.then((response) => response.data)
+			.then((aiResponse) => {
+				fetchBoard().then(() => {
+					playMoveAudio(aiResponse.aiMove.capture);
+          setLastMove(aiResponse.aiMove)
 
-          setLastAIResponse(aiResponse);
-        });
-      })
-      .finally(() => {
-        setIsWaitingForAI(false);
-      })
-  };
+					setLastAIResponse(aiResponse);
+				});
+			})
+			.finally(() => {
+				setIsWaitingForAI(false);
+			});
+	};
 
-  const movePiece = (pieceMove: TMove) => {
-    // setIsWaitingForAI(true);
-    
-    http
-      .post<TBoard>("/board/move/piece", pieceMove)
-      .then((response) => response.data)
-      .then((data) => {
-        setBoard(data);
+	const movePiece = (pieceMove: TMove) => {
+		setIsWaitingForAI(true);
 
-        // getAiMove();
-      })
-      .catch((err) => {
-        console.error(err);
-        setIsWaitingForAI(false);
-      })
-  };
+		http.post<TBoard>("/board/move/piece", pieceMove)
+			.then((response) => response.data)
+			.then((data) => {
+				setBoard(data);
 
-  const loadFEN = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+				getAiMove();
+			})
+			.catch((err) => {
+				console.error(err);
+				setIsWaitingForAI(false);
+			});
+	};
 
-    const inputFen: HTMLInputElement | null = document.getElementById(
-      "input-fen"
-    ) as HTMLInputElement;
+	const loadFEN = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-    if (!inputFen) return;
+		const inputFen: HTMLInputElement | null = document.getElementById("input-fen") as HTMLInputElement;
 
-    let fen = INITIAL_FEN;
-    if (inputFen.value.trim() !== "") {
-      fen = inputFen.value.trim();
-    }
+		if (!inputFen) return;
 
-    http
-      .post<TBoard>("/board/load/fen", {
-        fen,
-      })
-      .then((response) => response.data)
-      .then((data) => {
-        setBoard(data);
-        // if (!data.whiteMove) {
-        //     getAiMove();
-        // }
-        setLastAIResponse(undefined);
-      });
-  };
+		let fen = INITIAL_FEN;
+		if (inputFen.value.trim() !== "") {
+			fen = inputFen.value.trim();
+		}
 
-  const setAITime = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    let aiTimeInput = document.getElementById("ai_time");
+		http.post<TBoard>("/board/load/fen", {
+			fen,
+		})
+			.then((response) => response.data)
+			.then((data) => {
+				setBoard(data);
 
-    if (!aiTimeInput) {
-      return;
-    }
+				if (!data.whiteMove) {
+					getAiMove();
+				}
 
-    let time = Number((aiTimeInput as HTMLInputElement).value);
+				setLastAIResponse(undefined);
+			});
+	};
 
-    http
-      .post<{ moves: number, elapsedTime: number }>("/ai/time_to_think", {
-        time_to_think: time,
-      });
-  };
+	const setAITime = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		let aiTimeInput = document.getElementById("ai_time");
 
-  useEffect(() => {
-    fetchBoard();
-  }, []);
+		if (!aiTimeInput) {
+			return;
+		}
 
-  return (
-    <>
-      <div id="floating-forms">
-        <form method="post" onSubmit={loadFEN}>
-          <input type="text" name="fen" id="input-fen" />
-          <button type="submit" id="reset-btn">
-            Load FEN
-          </button>
-        </form>
-        {/* <form method="post" onSubmit={fetchCountMoves}>
+		let time = Number((aiTimeInput as HTMLInputElement).value);
+
+		http.post<{ moves: number; elapsedTime: number }>("/ai/time_to_think", {
+			time_to_think: time,
+		});
+	};
+
+	useEffect(() => {
+		fetchBoard();
+	}, []);
+
+	return (
+		<>
+			<div id="floating-forms">
+				<form method="post" onSubmit={loadFEN}>
+					<input type="text" name="fen" id="input-fen" />
+					<button type="submit" id="reset-btn">
+						Load FEN
+					</button>
+				</form>
+				{/* <form method="post" onSubmit={fetchCountMoves}>
           <input type="number" name="rawDepth" id="raw_search_depth" />
           <button type="submit" id="count_moves_btn">
             Count
           </button>
         </form>*/}
-        <form method="post" onSubmit={setAITime}>
-          <input type="number" name="aiTime" id="ai_time" defaultValue={2}/>
-          <button type="submit" id="set_ai_time_btn">
-            Set
-          </button>
-        </form> 
-      </div>
+				<form method="post" onSubmit={setAITime}>
+					<input type="number" name="aiTime" id="ai_time" defaultValue={2} />
+					<button type="submit" id="set_ai_time_btn">
+						Set
+					</button>
+				</form>
+			</div>
 
-      <div id="board">
-        <div id="white-captures" className="captures">
-          {board.whiteCaptures.map((piece_fen, i) => (
-            <img key={i} className="captured_piece" src={PIECE_ICONS[piece_fen]} alt={piece_fen} />
-          ))}
-        </div>
-        <div id="black-captures" className="captures">
-          {board.blackCaptures.map((piece_fen, i) => (
-            <img key={i} className="captured_piece" src={PIECE_ICONS[piece_fen]} alt={piece_fen} />
-          ))}
-        </div>
-        {LINES.map((i) => (
-          <div key={i} className="row">
-            {LINES.map((j) => {
-              const piece = board.pieces[i * 8 + j];
+			<div id="board">
+				<div id="white-captures" className="captures">
+					{board.whiteCaptures.map((piece_fen, i) => (
+						<img key={i} className="captured_piece" src={PIECE_ICONS[piece_fen]} alt={piece_fen} />
+					))}
+				</div>
+				<div id="black-captures" className="captures">
+					{board.blackCaptures.map((piece_fen, i) => (
+						<img key={i} className="captured_piece" src={PIECE_ICONS[piece_fen]} alt={piece_fen} />
+					))}
+				</div>
+				{LINES.map((i) => (
+					<div key={i} className="row">
+						{LINES.map((j) => {
+              const position = i * 8 + j;
 
-              return <div
-                key={j}
-                className={`cell${selectedPiece?.position === (i * 8 + j) ? " selected" : ""} ${getInCheckClass(board.blackKingInCheck, piece?.value, board.whiteKingInCheck)}`}
-                data-pos={i * 8 + j}
-                onClick={(e) => onMovePiece(e.currentTarget, i * 8 + j)}
-              >
-                <span className="cell-index">{i * 8 + j}</span>
-                {j === 0 && (
-                  <span className={`row-index ${(i + 1) % 2 === 0 ? "white" : ""}`}>{8 - i}</span>
-                )}
-                {i === 7 && (
-                  <span className={`column-index ${(j + 1) % 2 !== 0 ? "white" : ""}`}>
-                    {COLUMNS[j]}
-                  </span>
-                )}
-                {piece && piece.fen !== EMPTY_FEN ? (
-                  <BoardPiece 
-                    blackKingInCheck={board.blackKingInCheck} 
-                    boardPiece={piece} 
-                    onClick={onPieceSelect} 
-                    whiteKingInCheck={board.whiteKingInCheck}
-                  />
-                ) : (
-                  <div className="move-dot"></div>
-                )}
-              </div>
-            })}
-          </div>
-        ))}
-        {board.winner !== '-' && <span id="winner-announcement">{getBoardEvaluationMessage(board.boardEvaluation, board.winner)}</span>}
-      </div>
+							const piece = board.pieces[position];
 
-      <Logs aiResponse={lastAIResponse} board={board} isWaitingForAI={isWaitingForAI} />
-    </>
-  );
+							return (
+								<Cell
+                  key={position}
+									board={board}
+									column={j}
+                  lastMove={lastMove}
+									onClickPiece={onPieceSelect}
+									onMovePiece={onMovePiece}
+									piece={piece}
+									row={i}
+									selectedPiecePosition={selectedPiece?.position}
+								/>
+							);
+						})}
+					</div>
+				))}
+				{board.winner !== "-" && (
+					<span id="winner-announcement">
+						{getBoardEvaluationMessage(board.boardEvaluation, board.winner)}
+					</span>
+				)}
+			</div>
+
+			<Logs aiResponse={lastAIResponse} board={board} isWaitingForAI={isWaitingForAI} />
+		</>
+	);
 };
-
-const getInCheckClass = (blackKingInCheck: boolean, piece: number, whiteKingInCheck: boolean) => {
-  if(piece === (TPieceColor.Black | TPieceType.King) && blackKingInCheck) {
-    return 'in-check';
-  }
-
-  if(piece === (TPieceColor.White | TPieceType.King) && whiteKingInCheck) {
-    return 'in-check';
-  }
-
-  return '';
-}
 
 export default Board;
--- web-application/src/Board/constants.ts
+++ web-application/src/Board/constants.ts
@@ -1,3 +1,25 @@
+import { TPiece } from "./types";
+
 export const INITIAL_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 export const EMPTY_FEN = "-"
+
+export const LINES = [0, 1, 2, 3, 4, 5, 6, 7];
+export const COLUMNS: { [key: number]: string } = {
+  0: "A",
+  1: "B",
+  2: "C",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "G",
+  7: "H",
+};
+
+export const EMPTY_PIECE: TPiece = {
+  moves: [],
+  position: -1,
+  value: 0,
+  fen: null,
+  white: false,
+};
