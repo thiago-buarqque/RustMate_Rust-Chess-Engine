67d43b7b6f867aaefd159ab672088e66ffd856af
Thiago Buarque <thiago.buarqque@gmail.com>
2024-09-29 18:55:52 -0300

    Add perft tests

--- ai-engine/src/game_bit_board/_move/moves_counter.rs
+++ ai-engine/src/game_bit_board/_move/moves_counter.rs
@@ -11,9 +11,11 @@ pub fn count_moves(
 
     let result = _count_moves(board, depth, track_moves, move_generator, &mut lookup_table);
 
-    println!("Total: {result}");
+    if track_moves {
+        println!("Total: {result}");
 
-    println!("\nTime spent: {:#?}", start.elapsed());
+        println!("\nTime spent: {:#?}", start.elapsed());
+    }
 
     result
 }
@@ -62,41 +64,142 @@ fn _count_moves(
 }
 
 #[cfg(test)]
-mod tests {
+mod moves_counter_test {
+    use std::sync::Mutex;
+
+    use once_cell::sync::Lazy;
+
     use crate::game_bit_board::{
         _move::moves_counter::count_moves, board::Board,
         move_generator::move_generator::MoveGenerator,
     };
 
+    static MOVE_GENERATOR: Lazy<Mutex<MoveGenerator>> =
+        Lazy::new(|| Mutex::new(MoveGenerator::new()));
+
+    fn assert_fen_moves(fen: String, move_generator: &MoveGenerator, resuls: Vec<(usize, u64)>) {
+        println!("\nPerft position {fen}");
+        let mut board = Board::from_fen(fen.as_str());
+
+        for (depth, expected_result) in resuls {
+            print!("Depth {depth}: ");
+            assert_eq!(
+                count_moves(&mut board, depth, false, move_generator),
+                expected_result
+            );
+            println!("OK!");
+        }
+    }
+
     #[test]
-    fn test_move_generation_count() {
-        let mut board = Board::new();
-        let mut move_generator = MoveGenerator::new();
-        // Positions for initial FEN
+    fn test_perft_pos_1() {
+        let move_generator = MOVE_GENERATOR.lock().unwrap();
 
-        assert_eq!(count_moves(&mut board, 1, false, &mut move_generator), 20);
-        assert_eq!(count_moves(&mut board, 2, false, &mut move_generator), 400);
-        assert_eq!(
-            count_moves(&mut board, 3, false, &mut move_generator),
-            8_902
+        let mut results = Vec::new();
+
+        results.push((1, 20));
+        results.push((2, 400));
+        results.push((3, 8_902));
+        results.push((4, 197_281));
+        results.push((5, 4_865_609));
+
+        assert_fen_moves(
+            String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"),
+            &move_generator,
+            results,
         );
-        // assert_eq!(count_moves(&mut board, 4, false, &move_generator),
-        // 197_281); assert_eq!(
-        //     count_moves(&mut board, 5, false, &move_generator),
-        //     4_865_609
-        // );
-        // assert_eq!(
-        //     count_moves(&mut board, 6, false, &move_generator),
-        //     119_060_324
-        // );
+    }
 
-        // board
-        //     .load_position("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/
-        // PPPBBPPP/R3K2R w KQkq -");
+    #[test]
+    fn test_perft_pos_2() {
+        let move_generator = MOVE_GENERATOR.lock().unwrap();
 
-        // assert_eq!(count_moves(&mut board, 1, false), 48);
-        // assert_eq!(count_moves(&mut board, 2, false), 2_039);
-        // assert_eq!(count_moves(&mut board, 3, false), 97_862);
-        // assert_eq!(count_moves(&mut board, 4, false), 4_085_603);
+        let mut results = Vec::new();
+
+        results.push((1, 48));
+        results.push((2, 2_039));
+        results.push((3, 97_862));
+        results.push((4, 4_085_603));
+
+        assert_fen_moves(
+            String::from("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"),
+            &move_generator,
+            results,
+        );
+    }
+
+    #[test]
+    fn test_perft_pos_3() {
+        let move_generator = MOVE_GENERATOR.lock().unwrap();
+
+        let mut results = Vec::new();
+
+        results.push((1, 14));
+        results.push((2, 191));
+        results.push((3, 2_812));
+        results.push((4, 43_238));
+        results.push((5, 674_624));
+
+        assert_fen_moves(
+            String::from("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"),
+            &move_generator,
+            results,
+        );
+    }
+
+    #[test]
+    fn test_perft_pos_4() {
+        let move_generator = MOVE_GENERATOR.lock().unwrap();
+
+        let mut results = Vec::new();
+
+        results.push((1, 6));
+        results.push((2, 264));
+        results.push((3, 9_467));
+        results.push((4, 422_333));
+
+        assert_fen_moves(
+            String::from("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"),
+            &move_generator,
+            results,
+        );
+    }
+
+    #[test]
+    fn test_perft_pos_5() {
+        let move_generator = MOVE_GENERATOR.lock().unwrap();
+
+        let mut results = Vec::new();
+
+        results.push((1, 44));
+        results.push((2, 1486));
+        results.push((3, 62_379));
+        results.push((4, 2_103_487));
+
+        assert_fen_moves(
+            String::from("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"),
+            &move_generator,
+            results,
+        );
+    }
+
+    #[test]
+    fn test_perft_pos_6() {
+        let move_generator = MOVE_GENERATOR.lock().unwrap();
+
+        let mut results = Vec::new();
+
+        results.push((1, 46));
+        results.push((2, 2_079));
+        results.push((3, 89_890));
+        results.push((4, 3_894_594));
+
+        assert_fen_moves(
+            String::from(
+                "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            ),
+            &move_generator,
+            results,
+        );
     }
 }
